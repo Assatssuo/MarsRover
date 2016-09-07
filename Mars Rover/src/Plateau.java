cef732d7de1ad3d98ea74bf9ad9615fb18c46979
Assatssuo <jorge_jhdeqm@hotmail.com>
2016-09-06 22:32:42 -0300

    teste

---
+++ Mars Rover/src/Plateau.java
@@ -0,0 +1,11 @@
+
+public class Plateau {
+
+	private int[][] grid;
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Mars Rover/src/Rover.java
@@ -0,0 +1,20 @@
+import java.awt.Point;
+
+public class Rover {
+	private char direction;
+	
+	public Rover(char d){
+		direction = d;
+	}
+	
+	public void turn(char side){
+		switch(side){
+		case 'L':
+			break;
+		case 'R':
+			break;
+		}
+	}
+	
+	
+}
